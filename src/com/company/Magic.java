7f40c3f94cec2db9393ba5bddf7ecce7e8c96800
Akylbeksson <whitenazar@gmail.com>
2021-02-12 17:23:27 +0600

    Initial commit

---
+++ src/com/company/Magic.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Magic extends Hero {
+    @Override
+    public void applySuperAbility(String superAbilityType) {
+        System.out.println("Magician applied his super ability of MEGA POISONING");
+    }
+
+}
---
+++ src/com/company/Warrior.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Warrior extends Hero {
+    @Override
+    public void applySuperAbility(String superAbilityType) {
+        System.out.println("Warrior applied his super ability of CRITICAL DAMAGE");
+
+    }
+
+}
